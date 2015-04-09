c57d148cdb8c41a9d38f4a1560b2f1be93909197
RobertoBrandini <dp.sistemas@gmail.com>
2015-04-09 20:18:43 -0300

    Teste AD

--- test/app.js
+++ test/app.js
@@ -1,16 +1,24 @@
 var assert = require("assert");
 
 //Resolva o problema
-function problemaParaResolver(param){
-	return false;
+function buracoEmLetras(caracteres){
+	var count = 0;
+	for (var i = 0; i < caracteres.length; i++){
+		if(caracteres.charAt(0) === 'A' || caracteres.charAt(1)=== 'D')
+			count++;
+	}
+	return count;
 }
 
 //Descreva e teste
 //Para testar, execute: mocha
-describe('problemaParaResolver()', function(){
-  it('should return true', function(){
-    assert.equal(problemaParaResolver(0), true);
+describe('Buraco em Letras', function(){
+  it('deve retornar 1 quando passar A', function(){
+    assert.equal(buracoEmLetras('A'), 1);
+  })
+  it('deve retornar 2 quando passar AD', function(){
+    assert.equal(buracoEmLetras('AD'), 2);
   })
 })
 
-exports.problemaParaResolver = problemaParaResolver;
+exports.buracoEmLetras = buracoEmLetras;
